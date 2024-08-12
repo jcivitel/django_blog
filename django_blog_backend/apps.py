fca09490faf25ab3912e604abc24dc122e40eebd
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-08-12 09:52:42 +0200

    add project files

---
+++ django_blog_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoBlogBackendConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_blog_backend"
---
+++ django_blog_frontend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoBlogFrontendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_blog_frontend'
